82368cd2392736fffed3b434a0a1c5c2f3f811eb
Muhammad Handharbeni <mhandharbeni@gmail.com>
2018-03-18 10:16:39 +0700

    Assets mNews https://trello.com/c/bwG8rbYh/24-assets

--- app/src/main/java/com/mdirect/mnews/BaseApps.java
+++ app/src/main/java/com/mdirect/mnews/BaseApps.java
@@ -1,9 +1,15 @@
 package com.mdirect.mnews;
 
+import android.content.Context;
 import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 import com.mdirect.mnews.value.Constants;
+
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 /**
  * Created by Beni on 07/03/2018.
@@ -29,4 +35,19 @@ public class BaseApps extends AppCompatActivity{
     public String APP_SECRET = Constants.APP_SECRET;
     public String REDIRECT_URI = Constants.REDIRECT_URI;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        CalligraphyConfig.initDefault(new CalligraphyConfig
+                .Builder()
+                .setDefaultFontPath("fonts/Ubuntu_Regular.ttf")
+                .setFontAttrId(R.attr.fontPath)
+                .build()
+        );
+    }
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
+    }
 }
---
+++ databasemodule/src/main/java/illiyin/mhandharbeni/databasemodule/model/mnews/response/ResponseReportComment.java
@@ -0,0 +1,34 @@
+package illiyin.mhandharbeni.databasemodule.model.mnews.response;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by Beni on 16/03/2018.
+ */
+
+public class ResponseReportComment {
+
+    @SerializedName("success")
+    @Expose
+    private Boolean success;
+    @SerializedName("message")
+    @Expose
+    private String message;
+
+    public Boolean getSuccess() {
+        return success;
+    }
+
+    public void setSuccess(Boolean success) {
+        this.success = success;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
